6bc39f84edd7dd1dcf10e8d4f13a48efbf76bf98
enoonni <enoonni@gmail.com>
2026-01-23 15:04:02 +0300

    Add dto for telegram message

---
+++ src/telegram_bot/dto/message_context.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <cstdint>
+#include <string>
+
+namespace telegram_bot::dto
+{
+struct User
+{
+    int64_t id;
+    std::string username;
+    std::string first_name;
+    std::string last_name;
+    bool is_bot = false;
+};
+
+struct Chat
+{
+    int64_t id;
+    bool is_private = true;
+    std::string title;
+};
+
+enum class MessageType
+{
+    Text,
+    Command,
+    Photo,
+    Callback,
+    Unknown
+};
+
+struct MessageContext
+{
+    int64_t update_id;
+    int64_t message_id;
+    Chat chat;
+    User from;
+
+    MessageType type = MessageType::Unknown;
+    std::string text;
+    std::string payload;
+    int64_t timestamp;
+};
+} // namespace telegram_bot::dto
