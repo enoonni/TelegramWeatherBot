8b31e833df21369e54ef95e3410e99f9d17f28b2
enoonni <enoonni@gmail.com>
2026-02-01 23:56:18 +0300

    Add initialize logic main app

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "config/config.hpp"
+#include "database/database.hpp"
 #include "telegram_bot/telegram_bot.hpp"
 #include "telegram_bot/telegram_client_api/telegram_client_api.hpp"
 #include "utils/signal_handler.hpp"
@@ -6,18 +7,68 @@
 
 #include <chrono>
 #include <iostream>
+#include <locale>
 #include <thread>
 
-enum class App_State
+enum class AppState
 {
+    ReadConfigPath,
+    Initialize_db,
     Initialize,
     Ready
 };
 
 int main(int argc, char* argv[])
 {
+    utils::SignalHandler signal_handler;
+    AppState app_state = AppState::Initialize;
+    db::Database db;
+    std::string db_path = "telegram.db";
+    std::string config_path = "bot_config.json";
+    config::Config bot_config(config_path);
     weatherchecker::WeatherChecker checker;
-    checker.poll();
+
+    while (signal_handler.is_running())
+    {
+        switch (app_state)
+        {
+        case AppState::ReadConfigPath:
+            if (bot_config.load())
+            {
+                app_state = AppState::Initialize_db;
+            }
+            else
+            {
+                while (signal_handler.is_running())
+                {
+                }
+                return 0;
+            }
+            break;
+
+        case AppState::Initialize_db:
+            if (db.initialize(db_path))
+            {
+                app_state = AppState::Ready;
+            }
+            else
+            {
+                while (signal_handler.is_running())
+                {
+                    checker.poll();
+                }
+                return 0;
+            }
+            break;
+
+        case AppState::Ready:
+            break;
+
+        default:
+            break;
+        }
+    }
+
     std::string current_weather = checker.get_weather();
     std::cout << current_weather << "\n";
     // const std::string path = "bot_config.json";
